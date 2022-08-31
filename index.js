edc977ae2cbb14258b4f0a90f74b404a9828c975
Amazil <vip.noob.elf@gmail.ru>
2022-08-31 11:03:18 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const wrapper = document.querySelector('.wrapper');
+
+const items = [{
+        title: 'Title 1',
+        text: 'Paragraph 1',
+        colorText: 'red',
+    },
+    {
+        title: 'Title 2',
+        text: 'Paragraph 2',
+        colorText: 'blue',
+    },
+    {
+        title: 'Title 3',
+        text: 'Paragraph 3',
+        colorText: 'green',
+    }]
+    
+     for (let index = 0; index < items.length; index++) {
+        const p = document.createElement('p');
+        const titl = document.createElement('h1');
+         titl.innerHTML = items[index].title
+         p.innerHTML = items[index].text;
+         wrapper.appendChild(titl)
+         wrapper.appendChild(p);
+         p.style.color = items[index].colorText;
+         titl.style.color = items[index].colorText;
+         
+
+     }
+
+   
+
+
+
